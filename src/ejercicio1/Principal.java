14f9d9bb51fa979e358848a696e6edaee86c9a5f
andres.chapo <andres.chapo@bue.edu.ar>
2021-04-22 22:31:51 -0300

    Clase Archivo y Clase Personas

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -3,8 +3,29 @@ package ejercicio1;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hola");
+
+		Archivo archivo = new Archivo();
+		archivo.setRuta("Personas.txt"); //prueba.txt
+		
+		if(archivo.existe())
+		{
+			System.out.println("Archivo encontrado");
+		}
+		else
+		{
+			System.out.println("No existe archivo entonces lo creo");
+			archivo.creaArchivo();
+		}
+		
+		//ESCRITURA:
+
+//		archivo.escribe_lineas("\nUniversidad Tecnologica Nacional");
+	
+		//LECTURA:
+
+		
+		System.out.println("A CONTINUACIÓN LEE DE A LINEAS: ");
+		archivo.lee_lineas();
 	}
 
 }

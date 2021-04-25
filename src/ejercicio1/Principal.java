137439b1cb100bda138f1398560bfc3218f6aadc
andres.chapo <andres.chapo@bue.edu.ar>
2021-04-24 22:59:08 -0300

    Menu en Principal que permite procesar por etapas.

--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,42 +1,69 @@
 package ejercicio1;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 
 public class Principal {
 
 	public static void main(String[] args) {
-
+		Scanner in = new Scanner(System.in);
 		Archivo archivo = new Archivo();
-		archivo.setRuta("Personas.txt"); //prueba.txt
+
 		LinkedList<Persona> listadoPersonas = new LinkedList<Persona>();
 		LinkedList<String> listado = new LinkedList<String>();
 		Funciones fun = new Funciones();
 		
-		if(archivo.existe())
-		{
-			System.out.println("Archivo encontrado");
-		}
-		else
-		{
-			System.out.println("No existe archivo entonces lo creo");
-			archivo.creaArchivo();
+		
+		String opcion = "";
+		while(opcion !=  "0"){
+			 System.out.println("----------------------------");	
+			 System.out.println("1. Cargar archivo Personas.txt");
+			 System.out.println("2. Mostrar listadoPersonas");
+			 System.out.println("3. Guardar listadoPersonas en Resultado.txt");
+			 System.out.println("----------------------------");	
+			 System.out.println("Ingrese la opcion: ");	
+		     opcion = in.nextLine();
+			 System.out.println();
+			 switch (opcion) {
+				case "1":
+					archivo.setRuta("Personas.txt"); //prueba.txt
+					if(archivo.existe())
+					{
+						System.out.println("Archivo encontrado!");
+					}
+					else
+					{
+						System.out.println("No existe archivo, entonces se crea uno nuevo.");
+						archivo.creaArchivo();
+					}
+
+					archivo.cargaArchivoEnListado(listado);
+					fun.cargarListadoEnPersonas(listado, listadoPersonas);
+					System.out.println("Cargado y validado con exito!.");
+					System.out.println("PRESIONE CUALQUIER TECLA PARA CONTINUAR.");
+					in.nextLine();
+					break;
+				case "2":
+					System.out.println("---MOSTRANDO CONTENIDO DEL OBJETO listadoPersonas---");
+					for(Persona per:listadoPersonas) {
+						System.out.println(per.getNombre() +" "+ per.getApellido()+" "+ per.getDni());
+					}
+					System.out.println("---------");
+					System.out.println("PRESIONE CUALQUIER TECLA PARA CONTINUAR.");
+					in.nextLine();
+					break;
+				case "3":
+					//ESCRITURA:
+//					archivo.escribe_lineas("\nUniversidad Tecnologica Nacional");
+				default:
+					System.out.println("Datos invalidos!");
+					break;
+			 }
 		}
 		
 
-		//LECTURA:
-		System.out.println("A CONTINUACIÓN LEE DE A LINEAS: ");
-//		archivo.lee_lineas();
-		archivo.cargaArchivoEnListado(listado);
-		fun.cargarListadoEnPersonas(listado, listadoPersonas);
-		
-		System.out.println("---MOSTRANDO CONTENIDO DEL OBJETO listadoPersonas---");
-		for(Persona per:listadoPersonas) {
-			System.out.println(per.getNombre() +" "+ per.getApellido()+" "+ per.getDni());
-		}
-		System.out.println("---------");
-		//ESCRITURA:
-//		archivo.escribe_lineas("\nUniversidad Tecnologica Nacional");
+
 	}
 
 }
